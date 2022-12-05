277b3bf721947cb2f6cbbfe22b10b534ebbe2d10
Armend Azemi <armend@hldesign.se>
2022-12-05 17:12:47 +0100

    More work on the admin edit page

--- Dashboard/Administratörer/admin.js
+++ Dashboard/Administratörer/admin.js
@@ -1,5 +1,16 @@
 // This file is only for the pages that are under the Administratörer folder.
 
+const userData = {
+    name: document.querySelector("#name"),
+    lastName: document.querySelector("#lastName"),
+    emailPrimary: document.querySelector("#emailPrimary"),
+    emailSecondary: document.querySelector("#emailSecondary"),
+    mobile: document.querySelector("#mobile"),
+    phone: document.querySelector("#phone"),
+    adminStatus: document.querySelector("#adminStatus"),
+}
+let userDataHasChanged = false;
+
 // Authorization Section
 const authorizationSection = document.querySelector(".authorization");
 const dropdownAdmin = document.querySelector(".dropdown.admin");
@@ -28,6 +39,12 @@ function openAdminDropdownMenu() {
    }
 }
 
+function handleAdminSelection (event) {
+    dropdownAdminButton.querySelector('p').innerText = event.target.innerHTML;
+    openAdminDropdownMenu();
+
+}
+
 // Add new department section
 const currentAuth = document.querySelector(".current-auth");
 const addMoreDepartments = document.querySelector(".add-more-departments");
@@ -35,6 +52,7 @@ const addMoreDepartmentsDropdownWrapper = document.querySelector(".add-more-depa
 const addMoreDepartmentsDropDownMenu = document.querySelector(".add-more-departments .dropdown-menu");
 const dropDownItemsDepartment = document.querySelectorAll(".add-more-departments .dropdown-menu .dropdown-item");
 const addDepartmentButton = document.querySelector(".add-department-btn");
+const searchForDepartmentInput = addMoreDepartments.querySelector("input");
 const addTitle = document.querySelector(".add-title");
 const bottomButtons = document.querySelector(".bottom-buttons");
 const openDropdownMenuButton = addMoreDepartmentsDropdownWrapper.querySelector(".fa-chevron-down");
@@ -94,6 +112,30 @@ function handleAddDepartment(item) {
     });
 }
 
+function handleDepartmentSearch(event) {
+    if(!addMoreDepartmentsDropDownMenu.classList.contains("show")){
+        handleDepartmentDropdownMenu();
+    }
+    // Close when input is empty
+    if (event.target.value === "") {
+        handleDepartmentDropdownMenu();
+    }
+    // Filter the departments
+    let searchTerm = event.target.value.toLowerCase();
+    dropDownItemsDepartment.forEach(item => {
+      let departmentName = item.innerHTML.toLowerCase();
+        if (departmentName.includes(searchTerm)) {
+            item.style.backgroundColor = "#F8FCFD";
+        }else {
+            item.style.backgroundColor = "white";
+        }
+    });
+}
+
+/**
+ * Handles removing a department from the authTags div.
+ * @param {HTMLObjectElement} item - The department that is to be removed.
+ */
 function handleRemoveTag(tag) {
     tag.addEventListener("click", () => {
         authTags.removeChild(tag.parentNode);
@@ -106,15 +148,51 @@ function handleRemoveTag(tag) {
     });    
 }
 
-
-
-
-
-
-
+// ----------------
 // EVENT LISTENERS
+// ----------------
+
+// Admin Section
 dropdownAdminButton.addEventListener("click", openAdminDropdownMenu);
+dropdownMenuAdmin.querySelectorAll(".dropdown-item").forEach(item => item.addEventListener("click", handleAdminSelection));
+
+// Department Section
 addDepartmentButton.addEventListener("click", openDepartmentDropdownMenu);
 openDropdownMenuButton.addEventListener("click", handleDepartmentDropdownMenu);
 dropDownItemsDepartment.forEach((item) => handleAddDepartment(item));
+searchForDepartmentInput.addEventListener("input", (event) => handleDepartmentSearch(event));
 authTags.querySelectorAll(".fa-x").forEach((tagCloseButton) => handleRemoveTag(tagCloseButton));
+
+// Alert user if they have unsaved changes.
+window.onbeforeunload = function(e) {
+    if (userDataHasChanged)
+        return "You have unsaved changes. Are you sure you want to leave?"; 
+};
+
+
+
+
+//----------------
+//  MODALS
+//----------------
+// Delete admin modal
+const deleteAdminModal = document.querySelector(".delete-admin-modal");
+const adminSearch = document.querySelector(".search-for-admin");
+const adminSearchChevDown = adminSearch.querySelector(".fa-chevron-down");
+const dropdownResults = deleteAdminModal.querySelector(".dropdown-results");
+const deleteAdminModalFooter = deleteAdminModal.querySelector(".modal-footer");
+
+adminSearchChevDown.addEventListener("click", () => {
+    if (!dropdownResults.classList.contains("show")) {
+    dropdownResults.style.display = "block";
+    deleteAdminModalFooter.style.marginTop = 50 + dropdownResults.clientHeight + "px";
+    dropdownResults.classList.add("show");
+    adminSearchChevDown.style.transform = "rotate(180deg)";
+    } else {
+    dropdownResults.style.display = "none";
+    deleteAdminModalFooter.style.marginTop = "8px";
+    dropdownResults.classList.remove("show");
+    adminSearchChevDown.style.transform = "rotate(0deg)";
+    }
+});
+
